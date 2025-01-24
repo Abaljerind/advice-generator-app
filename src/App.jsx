f2653d89a4152787387bcd3d886805671cbdc8b3
Abaljerind <abaljerinda@gmail.com>
2025-01-24 11:17:30 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div>
+        <p className="bg-teal-950 text-3xl font-bold underline">Hello there</p>
+      </div>
+    </>
+  );
+}
+
+export default App;

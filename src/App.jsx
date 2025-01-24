ea4027494039fa57e695feed0bf260852de9f533
Abaljerind <abaljerinda@gmail.com>
2025-01-24 22:19:11 +0700

    Add state for quote and quoteId
    Create handleGetQuote() to fetch data from api and setQuote and setQuoteId
    Add useEffect to run handleGetQuote() once after the page loaded
    Change style of width for div tag
    Change style in hover state for button
    Change number as static data with quoteId for dynamic data based on api
    Add event onClick to button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [quote, setQuote] = useState("");
+  const [quoteId, setQuoteId] = useState(null);
+
+  async function handleGetQuote() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setQuote(data.slip.advice);
+    setQuoteId(data.slip.id);
+  }
+
+  useEffect(() => {
+    handleGetQuote();
+  }, []);
+
   document.body.style.backgroundColor = "rgb(31, 38, 50)";
 
   return (
-    <div className="font-manrope relative mx-auto flex h-screen w-[345px] flex-col items-center justify-center px-2">
+    <div className="font-manrope relative mx-auto flex h-screen max-w-[345px] flex-col items-center justify-center px-2">
       <section className="bg-darkGrayishBlue rounded-xl px-1 py-12 text-center">
-        <p className="text-green mb-6 text-sm tracking-[0.2em]">ADVICE #117</p>
+        <p className="text-green mb-6 text-sm tracking-[0.2em]">
+          ADVICE #{quoteId}
+        </p>
         <q className="text-cyan text-[24px]/8 font-bold">{quote}</q>
         <img
           src="src/assets/pattern-divider-mobile.svg"
@@ -12,7 +30,10 @@ function App() {
           className="mx-auto mt-8 mb-4 w-[90%]"
         />
       </section>
-      <button className="bg-green -mt-8 flex h-16 w-16 cursor-pointer items-center justify-center rounded-full transition duration-300 ease-in-out hover:brightness-125">
+      <button
+        className="bg-green hover:shadow-green/50 -mt-8 flex h-16 w-16 cursor-pointer items-center justify-center rounded-full transition duration-300 ease-in-out hover:shadow-xl"
+        onClick={handleGetQuote}
+      >
         <img
           src="src/assets/icon-dice.svg"
           alt="icon dice"
